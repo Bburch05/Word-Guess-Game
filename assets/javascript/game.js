2ce8e6b7be98dcc8a0fae4718acd08e089bb2c91
Brian Burch <Bburch05@gmail.com>
2018-06-03 09:26:30 -0400

    Changes made

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,32 @@
+function initialize() {
+    hangman.playCorrectGuesses = [];
+    for (var i = 0; i < comGuess.length; i++) {
+        hangman.playCorrectGuesses.push('_');
+      }
+    console.log(hangman.playCorrectGuesses);
+    hangman.guessLeft = hangman.guesses;
+    hangman.playWrongGuesses = [];
+};
+
 var hangman = {
     wins : 0,
     losses : 0,
     guesses : 5,
     guessLeft : 5,
-    guessList: ["Dark Souls", "Front Mission", "Super Mario Bros", "Final Fantasy", "Mario"],
+    guessList: ["dark-souls", "front-mission", "super-mario-bros", "final-fantasy", "mario"],
     //The Computer Chooses a Random word from the List
     playCorrectGuesses : [],
     //store Wrong Guesses
-    playWrongGuesses : []
+    playWrongGuesses : [],
+    checkWin: function (){
+        if (hangman.playCorrectGuesses.indexOf("_") === -1)
+            hangman.wins++
+            
+    
+        if (hangman.guessLeft <= 0) {
+            hangman.losses++
+        }
+    }
 };
 
 var DarkSouls = {
@@ -15,37 +34,37 @@ var DarkSouls = {
     img: ""
 };
 
-function initialize() {
-    hangman.playCorrectGuesses = [];
-    var comGuess = hangman.guessList[Math.floor(Math.random()*hangman.guessList.length)];
-    console.log(comGuess);
+var comGuess = hangman.guessList[Math.floor(Math.random()*hangman.guessList.length)];
+console.log(comGuess);
 
-    for (var i = 0; i < comGuess.length; i++) {
-        hangman.playCorrectGuesses.push('_');
-      }
-    console.log(hangman.playCorrectGuesses);
-    hangman.guessLeft = 5;
-    hangman.playWrongGuesses = [];
-};
+
 
     initialize();
   
-document.onkeyup = function(event) {
+document.onkeydown = function(event) {
     $("#winCount").text(hangman.wins);
     $("#lossCount").text(hangman.losses);
     $("#guessLeft").text(hangman.guessLeft);
-    $("#lettersRight").text(hangman.playCorrectGuesses);
-    $("#lettersWrong").text(hangman.playWrongGuesses);
-    
+    console.log(comGuess);
 
-    initialize();
+    if (comGuess.indexOf(event.key) === -1 ) {
+        hangman.playWrongGuesses.push(event.key);
+        $("#lettersWrong").text(hangman.playWrongGuesses);
+        hangman.guessLeft--
+        hangman.checkWin();
+    }
+    else { 
+        for (j = 0; j < comGuess.length ; j++){
+            if (event.key === comGuess[j]){
+                hangman.playCorrectGuesses[j] = comGuess[j];
+                $("#lettersRight").text(hangman.playCorrectGuesses);
+                hangman.checkWin();
+            }
+        }
+    }
 
-    //The Computer hides it's guess
-    if (hangman.comGuess.indexOf(event.key) === -1 )
-        hangman.playWrongGuesses = push(event.key)
-    else 
-        hangman.playCorrectGuesses.forEach(function(item, i) { if (item == 3452) a[i] = 1010; });
 
+  
 // if (hangman.playCorrectGuesses.indexOf('_') = -1){
 //     hangman.wins++;
 //     $(".winorlose").text("You Win!");
