a9fa1dd962265ba47ba833f57d50be6958a58a85
Brian Burch <Bburch05@gmail.com>
2018-06-03 23:10:00 -0400

    Changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,46 @@
 function initialize() {
     hangman.playCorrectGuesses = [];
-    for (var i = 0; i < comGuess.length; i++) {
+    for (var i = 0; i < hangman.comGuess.length; i++) {
+        if (hangman.comGuess.charAt[i] != "-"){
         hangman.playCorrectGuesses.push('_');
-      }
-    console.log(hangman.playCorrectGuesses);
+        }
+    }
     hangman.guessLeft = hangman.guesses;
     hangman.playWrongGuesses = [];
+    hangman.comGuess = hangman.guessList[Math.floor(Math.random()*hangman.guessList.length)];
 };
+
+var worddisplay = "";
 
 var hangman = {
     wins : 0,
     losses : 0,
     guesses : 5,
     guessLeft : 5,
-    guessList: ["dark-souls", "front-mission", "super-mario-bros", "final-fantasy", "mario"],
+    guessList: ["Dark-Souls", "Front-Mission", "Super-Mario-Bros", "Final-Fantasy", "Mario"],
     //The Computer Chooses a Random word from the List
-    playCorrectGuesses : [],
+    playCorrectGuesses : [], 
     //store Wrong Guesses
     playWrongGuesses : [],
+    comGuess : "" ,
     checkWin: function (){
         if (hangman.playCorrectGuesses.indexOf("_") === -1)
             hangman.wins++
+            $(".winorlose").text("You Win!");
+            
             
     
         if (hangman.guessLeft <= 0) {
             hangman.losses++
+            $(".winorlose").text("You Lose! The answer was " + hangman.comGuess + ".");
         }
     }
 };
 
-var DarkSouls = {
-    title: "Dark Souls",
-    img: ""
-};
 
-var comGuess = hangman.guessList[Math.floor(Math.random()*hangman.guessList.length)];
-console.log(comGuess);
+console.log(hangman.comGuess);
 
-
+//Guess ul
 
     initialize();
   
@@ -45,18 +48,19 @@ document.onkeydown = function(event) {
     $("#winCount").text(hangman.wins);
     $("#lossCount").text(hangman.losses);
     $("#guessLeft").text(hangman.guessLeft);
-    console.log(comGuess);
+    console.log(hangman.comGuess);
 
-    if (comGuess.indexOf(event.key) === -1 ) {
+    if (hangman.comGuess.indexOf(event.key) === -1 ) {
         hangman.playWrongGuesses.push(event.key);
         $("#lettersWrong").text(hangman.playWrongGuesses);
         hangman.guessLeft--
         hangman.checkWin();
     }
     else { 
-        for (j = 0; j < comGuess.length ; j++){
-            if (event.key === comGuess[j]){
-                hangman.playCorrectGuesses[j] = comGuess[j];
+        for (j = 0; j < hangman.comGuess.length ; j++){
+            if (event.key == hangman.comGuess[j]){
+                hangman.playCorrectGuesses[j] = hangman.comGuess[j];
+                worddisplay.charAt = ""
                 $("#lettersRight").text(hangman.playCorrectGuesses);
                 hangman.checkWin();
             }
@@ -67,13 +71,13 @@ document.onkeydown = function(event) {
   
 // if (hangman.playCorrectGuesses.indexOf('_') = -1){
 //     hangman.wins++;
-//     $(".winorlose").text("You Win!");
+//     
 //     initialize();
 // }
 
 // else if (hangman.playWrongGuesses.length = hangman.guesses) {
-//     hangman.losses++;
-//     $(".winorlose").text("You Lose! The answer was " + comGuess + ".");
+//     
+//     
 //     initialize();
 // }
 
